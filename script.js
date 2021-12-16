f2bcfdab548836626194ea5aaeaf7a2891e9397f
MarcoLotti <gorlandelli64@gmail.com>
2021-12-16 12:00:59 +0100

    carousel fatto (mancano i testi e css)

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const imagesArray = [
+
+const items = [
     'img/01.jpg',
     'img/02.jpg',
     'img/03.jpg',
@@ -6,16 +7,32 @@ const imagesArray = [
     'img/05.jpg'
 ];
 
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+];
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
 
 let itemsContent = '';
 let elementActive = 0;
 
-for (let i=0; i< imagesArray.length; i++) {
+for (let i=0; i< items.length; i++) {
     itemsContent += `
         <div class="item" id="item-${i}">
-            <img src="${imagesArray[i]}">
+            <img src="${items[i]}">
         </div>`;
-}
+} 
 
 const itemsContainer = document.querySelector('.item-container');
 itemsContainer.innerHTML = itemsContent;
@@ -27,13 +44,21 @@ const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
 prev.addEventListener('click', function() {
+    if(elementActive < 1){
+        elementActive = 5;
+    }
     elementActive--;
+    console.log(elementActive);
     itemActive.classList.remove('active');
     itemActive = document.getElementById('item-'+elementActive);
     itemActive.classList.add('active');
 });
 next.addEventListener('click', function() {
-    elementActive++;
+    if(elementActive > 3){
+        elementActive = -1;
+    }
+    elementActive++;  
+    console.log(elementActive);
     itemActive.classList.remove('active');
     itemActive = document.getElementById('item-'+elementActive);
     itemActive.classList.add('active');
